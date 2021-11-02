3ba34ea247edcc6bb8b6b5a814809113ad02aa1f
ApacheAlpha <1706670065@qq.com>
2021-11-02 18:15:54 +0800

    提交测试代码

--- entity.js
+++ entity.js
@@ -1,10 +1,14 @@
 const { Parse } = require('./test/parse')
 // 实体组抽象类
 class EntityGroup extends Parse.Object {
-	constructor() {
-		super()
+	constructor(...args) {
+		super(...args)
+		console.log('))constructor)))',args)
 		// this.name = this.constructor.name
 		// this.insert_list = []
+	}
+	static get Permissions() {
+		return ['read', 'write']
 	}
 
 	// 把实体添加到当前实体组的fieldName数组字段里，并设置好权限规则
@@ -16,15 +20,37 @@ class EntityGroup extends Parse.Object {
 			return
 		}
 
-		try {
-			const query_data = new Parse.Query(this.name)
-			const query_data_list = await query_data.find({ useMasterKey: true })
-			const relation = query_data_list[0].relation(fieldName)
-			relation.add(entity)
-			query_data_list[0].save()
-		} catch (error) {
-			console.log('addEntity error: ', error)
-		}
+		const org_id = this.organization.id
+		// const query_data = new Parse.Query(Parse.Role)
+		// query_data.containedIn('name',[`Organization__${org_id}__read__Inventory`, `Organization__${org_id}__write__Inventory`])
+		// const query_data_list = await query_data.find({ useMasterKey: true })
+
+		const roleACL = new Parse.ACL()
+		roleACL.setRoleReadAccess(`Organization__${org_id}__grant`,true)
+		roleACL.setRoleWriteAccess(`Organization__${org_id}__grant`,true)
+		const role_A = new Parse.Role(`Organization__${org_id}__read__Inventory`, roleACL)
+		const role_B = new Parse.Role(`Organization__${org_id}__write__Inventory`, roleACL)
+		await	role_A.save()
+		await	role_B.save()
+
+		const inventory_roleACL = new Parse.ACL()
+		inventory_roleACL.setRoleWriteAccess(`Organization__${org_id}__read__Inventory`,true)
+		inventory_roleACL.setRoleReadAccess(`Organization__${org_id}__write__Inventory`,true)
+		entity.setACL(inventory_roleACL)
+		await entity.save()
+
+		const relation = this.organization.relation(fieldName)
+		relation.add(entity)
+		await this.organization.save()
+	}
+
+	// 把实体从当前实体组的fieldName数组字段里删除
+	// fieldName：String，字段名
+	// entity：Parse.Object，实体
+	async removeEntity(fieldName, entity) {
+		const relation = this.organization.relation(fieldName)
+		relation.remove(entity)
+		await this.organization.save()
 	}
 
 	// 把实体组添加到当前实体组fieldName数组字段里，并设置好权限规则
@@ -35,64 +61,103 @@ class EntityGroup extends Parse.Object {
 			console.log('typeof error:entity need a ( object or array) or fieldName Must be entered')
 			return
 		}
+		const current_user = 	Parse.User.current()
+		const org_id = this.organization.id
 		const roleACL = new Parse.ACL()
-		roleACL.setRoleReadAccess('Organization-8CmJyfFubz-read-Organization',true)
-		roleACL.setRoleWriteAccess('Organization-8CmJyfFubz-write-Organization',true)
-		roleACL.setRoleReadAccess('Organization-8CmJyfFubz-write-Organization',true)
-		entityGroup.setACL(roleACL)
-		await	entityGroup.save()
+		roleACL.setRoleReadAccess(`Organization__${org_id}__grant`,true)
+		roleACL.setRoleWriteAccess(`Organization__${org_id}__grant`,true)
+		const role_A = new Parse.Role(`Organization__${org_id}__read__Project`, roleACL)
+		const role_B = new Parse.Role(`Organization__${org_id}__write__Project`, roleACL)
+		// role_A.getUsers().add(current_user)
+		// role_B.getUsers().add(current_user)
+
+		await role_A.save()
+		await role_B.save()
+
+		const roleACL_1 = new Parse.ACL()
+		roleACL_1.setRoleReadAccess(`Organization__${org_id}__read__Project`,true)
+		roleACL_1.setRoleWriteAccess(`Organization__${org_id}__write__Project`,true)
+		entityGroup.setACL(roleACL_1)
+		await entityGroup.save()
 
 		const relation = this.organization.relation(fieldName)
 		relation.add(entityGroup)
 		await this.organization.save()
 	}
 
+	// 把实体组从当前实体组fieldName数组字段里删除，并设置好权限规则
+	// 实体组下的资源也会被删除
+	// fieldName：String，字段名
+	// entityGroup：EntityGroup，实体组
+	async removeEntityGroup(fieldName, entityGroup) {
+		const devices = entityGroup.attributes.devices
+		// 从下往上删除 未完成
+		const org = this.organization
+		const relation = org.relation(fieldName)
+		relation.remove(entityGroup)
+		org.save()
+
+		const query = new Parse.Query(Parse.Role)
+		query.containedIn('name',[`Organization.${org.id}.read.Project`,`Organization.${org.id}.write.Project`,`Project.${org.id}.read.Device`,`Project.${org.id}.write.Device`])
+		const role_list = await query.find({useMasterKey:true})
+		for (let index = 0; index < role_list.length; index++) {
+			array[index].destroy()
+		}
+
+		entityGroup.destroy()
+	}
+
 	// 向实体组添加成员，创建对应的角色权限，并添加此成员到角色里
 	// user: Parse.User，要添加的用户
 	// permissions：Array<String>，该用户在当前实体拥有的权限
-	async addMembers(user, permissions = ['read', 'write']) {
+	// withGrant: 是否分配权限控制权限，子实体组此参数不能为true
+	async addMembers(user,permissions = EntityGroup.Permissions, withGrant) {
+		const org_id = this.organization.id
 		const relation = this.organization.relation('numbers')
 		relation.add(user)
 		await this.organization.save()
 
 		const roleACL = new Parse.ACL()
 		for (let index = 0; index < permissions.length; index++) {
-			if (array[index]==='read') {
-				const read_Role = new Parse.Role(`Organization-${this.organization.id}-read-user`, roleACL)
+			if (permissions[index] === 'read') {
+				const read_Role = new Parse.Role(`Organization.${org_id}.read-user`, roleACL)
 				read_Role.getUsers().add(user)
 				await read_Role.save()
 			}
-			if (array[index]==='write') {
-				const write_Role = new Parse.Role(`Organization-${this.organization.id}-write-user`, roleACL)
+			if (permissions[index] === 'write') {
+				const write_Role = new Parse.Role(`Organization.${org_id}.write-user`, roleACL)
 				write_Role.getUsers().add(user)
 				await write_Role.save()
 			}
 		}
+
+		if (withGrant) {
+			const withGrant_roleACL = this.organization.getACL()
+			withGrant_roleACL.setReadAccess(user.id,true)
+			withGrant_roleACL.setWriteAccess(user.id,true)
+			withGrant_roleACL.setACL(withGrant_roleACL)
+			await withGrant_roleACL.save()
+		}
 	}
 
 	// 删除实体组成员，并在对应的角色权限中删除此成员
-	// user: Parse.User，要删除的用户
+	// user: Parse.User，要添加的用户
 	// permissions：Array<String>，该用户在当前实体拥有的权限
 	async delMembers(user) {
-		const query_data = new Parse.Query(this.name)
-		const organization_result = await query_data.find({ useMasterKey: true })
-		organization_result[0].remove(user)
+		const org = this.organization
+		const relation =org.relation('members')
+		relation.remove(user)
+		await org.save()
 
-		const organization_result_acl = organization_result[0].getACL()
-		organization_result_acl.setReadAccess(user, false)
-		organization_result_acl.setWriteAccess(user, false)
-		organization_result[0].setACL(organization_result_acl)
-		organization_result[0].save()
+		const user_role_query = new Parse.Query(Parse.Role)
+		user_role_query.containedIn('name',[`Organization.${org_id}.write-user`,`Organization.${org_id}.read-user`])
+		const user_role_list = 	user_role_query.find({useMasterKey:true})
 
-		const role_query = new Parse.Query(Parse.Role)
-		role_query.startsWith('name', 'Organization')
-		role_query.endsWith('name', 'Organization')
-		const role_list = await role_query.find({ useMasterKey: true })
-		for (let index = 0; index < role_list.length; index++) {
+		for (let index = 0; index < user_role_list.length; index++) {
 			const role_acl = role_list[index].getACL()
 			role_acl.setReadAccess(user, false)
 			role_acl.setWriteAccess(user, false)
-			role_list[index].setACL(organization_result_acl)
+			role_list[index].setACL(role_acl)
 			role_list[index].save()
 		}
 	}
@@ -100,7 +165,19 @@ class EntityGroup extends Parse.Object {
 	// 改变某实体组成员的权限
 	// user: Parse.User，要改变的用户
 	// permissions：Array<String>，该用户在当前实体拥有的权限
-	async setMemberPermission(user, permissions = ['read', 'write']) {
+	async addMemberPermission(user, permissions = EntityGroup.Permissions, withGrant) {
+		const role_query = new Parse.Query(Parse.Role)
+		const role_list = await role_query.find({ useMasterKey: true })
+		for (let index = 0; index < role_list.length; index++) {
+			const role_acl = role_list[index].getACL()
+			role_acl.setReadAccess(user, true)
+			role_acl.setWriteAccess(user, true)
+			role_list[index].setACL(organization_result_acl)
+			role_list[index].save()
+		}
+	}
+
+	async ReduceMemberPermission(user, permissions = ['read', 'write']) {
 		const role_query = new Parse.Query(Parse.Role)
 		const role_list = await role_query.find({ useMasterKey: true })
 		for (let index = 0; index < role_list.length; index++) {
@@ -109,39 +186,6 @@ class EntityGroup extends Parse.Object {
 			role_acl.setWriteAccess(user, false)
 			role_list[index].setACL(organization_result_acl)
 			role_list[index].save()
-		}
-	}
-
-	async unsetMemberPermission(user, permissions = ['read', 'write']) {
-		const role_query = new Parse.Query(Parse.Role)
-		const role_list = await role_query.find({ useMasterKey: true })
-		for (let index = 0; index < role_list.length; index++) {
-			const role_acl = role_list[index].getACL()
-			role_acl.setReadAccess(user, false)
-			role_acl.setWriteAccess(user, false)
-			role_list[index].setACL(organization_result_acl)
-			role_list[index].save()
-		}
-	}
-
-	// 授予用户改变角色权限的权限
-	// 设置当前实体组相关角色权限记录的ACL，使指定用户能更改该记录
-	// user: Parse.User，要授予的用户
-	async grantMemberChomdPermission(user) {
-		const query_data = new Parse.Query(Parse.Role)
-		const role_list = await query_data.find({ useMasterKey: true })
-		for (let index = 0; index < role_list.length; index++) {
-			console.log('))))))))))))))))))',			role_list[index]
-			)
-			// if (role_list[index].) {
-				
-			// }
-
-			// const acl = new Parse.ACL()
-			// acl.setReadAccess(user, true)
-			// acl.setWriteAccess(user, true)
-			// role_list[index].setACL(acl)
-			// role_list[index].save()
 		}
 	}
 }
--- test/main.js
+++ test/main.js
@@ -2,80 +2,166 @@ const entity = require('../entity')
 const { Parse } = require('./parse')
 
 
-class organization extends entity.EntityGroup {
-	constructor() {
-		super()
-		this.organization = null
-	}
+// class organization extends entity.EntityGroup {
+// 	constructor() {
+// 		super()
+// 		this.organization = null
+// 	}
 
-	addProject(project) {
-		return super.addEntityGroup('project', project)
-	}
+// 	addProject(project) {
+// 		return super.addEntityGroup('projects', project)
+// 	}
 
-	addMembers(user) {
-		return super.addMembers(user, permissions = ['read', 'write'])
-	}
+// 	addEntity(entity) {
+// 		return super.addEntity('projets', entity)
+// 	}
 
-	grantMemberChomdPermission(user) {
-	 return super.grantMemberChomdPermission(user)
-	}
 
-}
+// 	addMembers(user) {
+// 		return super.addMembers(user, permissions = ['read', 'write'])
+// 	}
+
+// 	removeEntityGroup(entityGroup) {
+// 		return super.removeEntityGroup('projects', entityGroup)
+// 	}
+
+// }
 
 // class Project extends Parse.EntityGroup {
 // 	addDevice(device) {
 // 	  return super.addEntity('devices', device)
 // 	}
+
 // }
 
 async function test() {
-	const org = new organization()
-	const Project_obj = new Parse.Object('Project')
-	Project_obj.set('Project_name','测试000')
-	Project_obj.set('Project_address','测试0001')
-	// await Project_obj.save()
+	// console.log('))))))))))', org_id)
+	// console.log('))))))))))', EntityGroup.Permissions)
 
-	const organization_query = new Parse.Query('organization')
-	const organization_list = await organization_query.find({ useMasterKey: true })
-	org.organization = organization_list[0]
+
+	// const org = new organization()
+	// const Project_obj = new Parse.Object('Project')
+	// Project_obj.set('Project_name', '测试000')
+	// Project_obj.set('Project_address', '测试0001')
+	// // await Project_obj.save()
+
+	// const organization_query = new Parse.Query('organization')
+	// const organization_list = await organization_query.find({ useMasterKey: true })
+	// org.organization = organization_list[0]
 	// org.addProject(Project_obj)
 
-
-	const user_query = new Parse.Query(Parse.User)
-	const user_list = await user_query.find({ useMasterKey: true })
+	// const user_query = new Parse.Query(Parse.User)
+	// const user_list = await user_query.find({ useMasterKey: true })
 	// org.addMembers(user_list[0])
 
-	org.grantMemberChomdPermission(user_list[0])
-
-
+	// org.grantMemberChomdPermission(user_list[0])
 
 	// const relation = organization_list[0].relation('LLLLLL')
 	// relation.add(Project_obj)
 	// organization_list[0].save()
 
-	// const Project_query = new Parse.Query('Project')
-	// const Project_query = new Parse.Query(Parse.Role)
+	// const org_id = '001'
+	// const query_data = new Parse.Query(Parse.Role)
+	// query_data.containedIn('name', [`Organization.${org_id}.read`, `Organization.${org_id}.write`, `Organization.${org_id}.grant`])
+	// const query_data_list = await query_data.find({ useMasterKey: true })
+	// console.log('LLLLLLLLLLLLLLLL', query_data_list)
+	// console.log('LLLLLLLLLLLLLLLL', query_data_list[0].get('name'))
 
-	// const Project_list = await Project_query.find({ useMasterKey: true })
-	// console.log('-------Project_list--',Project_list)
-	// console.log('-------Project_list--',await Project_list[1].getACL())
+	// const org = new organization()
+	// const inv_obj = new Parse.Object('inv')
+	// inv_obj.set('inv_name', '测试000')
+	// await inv_obj.save()
+
+	// const organization_query = new Parse.Query('Organization')
+	// const organization_list = await organization_query.find({ useMasterKey: true })
+	// console.log('LLLLLLLLLLLLLLLL', organization_list)
+	// org.organization = organization_list[0]
+	// org.addEntity(inv_obj)
+
+	// Organization__8CmJyfFubz__read__Organization
+	// Organization__8CmJyfFubz__write__Organization
+	// Organization__8CmJyfFubz__grant
+
+	// const role_list = new Parse.Query('Project')
+	// const result = await role_list.find({useMasterKey:true})
+	// console.log('))))))))234)))))))))',result[0])
+
+
 	
+	// const Project1 = new Parse.Query('Project1')
+	// const Project1_result =await Project1.find({useMasterKey:true})
+	// console.log('Project1_resultProject1_result',Project1_result)
+
+	// const Project1 = new Parse.Object('Project')
+	// Project1.set('Project_name','uuuuuuu')
+	// Project1.set('Project_address','KKKKKKKKKKK')
+	// await Project1.save()
+
+	// const [project] =await new Parse.Query('Project').find()
+	// console.log('))))))77777)234234))))))))))', project)
+	// const [device] =await new Parse.Query('device').find()
+	// console.log('))))))77777)2))))))))))',device)
+	// project.relation('devices').add(device)
+	// await project.save()
+
+
+	const Project_query =new Parse.Query('Project')
+	const result = await Project_query.find()
+	console.log('))))))))0))))))))',result)
+	
+	const user_query =new Parse.Query(Parse.User)
+	const user_result = await user_query.find()
+	console.log('))))))))1))))))))',user_result)
+
+	
+	// const acl = new Parse.ACL()
+	// acl.setReadAccess(result, true)
+	// acl.setWriteAccess(result, true)
+
+	// const relation = result[0].relation('devices')
+	// const DDD =await relation.query().find({useMasterKey:true})
+	// console.log(')))))))))23123123))))))))',DDD)
+
+	// result[0].remove()
+
+
+
 
 }
-
 test()
 
 
-async function test_addProject() {
-	const org = new organization()
-	const Project_obj = new Parse.Object('Project')
-	Project_obj.set('Project_name','测试000')
-	Project_obj.set('Project_address','测试0001')
-	await Project_obj.save()
+// async function test_addProject() {
+// 	const org = new organization()
+// 	const Project_obj = new Parse.Object('Project')
+// 	Project_obj.set('Project_name', '测试000')
+// 	Project_obj.set('Project_address', '测试0001')
+// 	await Project_obj.save()
 
-	const organization_query = new Parse.Query('organization')
-	const organization_list = await organization_query.find({ useMasterKey: true })
-	org.organization = organization_list[0]
-	org.addProject(Project_obj)
-}
+// 	const organization_query = new Parse.Query('organization')
+// 	const organization_list = await organization_query.find({ useMasterKey: true })
+// 	org.organization = organization_list[0]
+// 	org.addProject(Project_obj)
+// }
+// // test_addProject()
 
+// async function test_addEntity() {
+// 	const org = new organization()
+// 	const inv_obj = new Parse.Object('inventory')
+// 	inv_obj.set('sn', '测试000')
+// 	await inv_obj.save()
+
+// 	const organization_query = new Parse.Query('organization')
+// 	const organization_list = await organization_query.find({ useMasterKey: true })
+// 	org.organization = organization_list[0]
+// 	org.addEntity(inv_obj)
+// }
+
+// async function removeEntityGroup() {
+// 	const org = new organization()
+// 	const Project_query = new Parse.Query('Project')
+// 	const Project_list = await Project_query.find({ useMasterKey: true })
+// 	org.removeEntityGroup(Project_list[0])
+
+// }
+// // removeEntityGroup()
